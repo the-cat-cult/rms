5eed3a04414e784e11dd5d11b0e46e3575419c96
sooryasrajan <soorya.s27@gmail.com>
2023-08-31 01:53:38 +0530

    Fixed more stuff

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", express.static('public'))
 // set up mongoose
-//mongodb+srv://root:root@cluster0.sagez.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect('mongodb://localhost:27017/rms', { dbName: 'rms' })
+mongoose.connect('mongodb+srv://root:root@cluster0.sagez.mongodb.net/?retryWrites=true&w=majority', { dbName: 'rms' })
     .then(() => {
         console.log('Database connected');
     })
